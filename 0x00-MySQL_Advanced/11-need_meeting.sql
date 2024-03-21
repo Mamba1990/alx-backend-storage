1779389abd7ecc39e1bb55c81f4b11b5b76924ae
Mamba1990 <h.daoudim@outlook.com>
2024-03-21 23:41:05 +0000

    task11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,15 +1,12 @@
--- creates a function SafeDiv that divides (and returns) the first
--- by the second number or returns 0 if the second number is equal to 0.
-DROP FUNCTION IF EXISTS SafeDiv;
-DELIMITER $$
-CREATE FUNCTION SafeDiv (a INT, b INT)
-RETURNS FLOAT DETERMINISTIC
-BEGIN
-    DECLARE result FLOAT DEFAULT 0;
-
-    IF b != 0 THEN
-        SET result = a / b;
-    END IF;
-    RETURN result;
-END $$
-DELIMITER ;
+-- Creates a view need_meeting that lists all students that have a score
+-- under 80 (strict) and no last_meeting or more than 1 month.
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+            (
+                last_meeting IS NULL
+                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+            )
+;
