51db84da56a4a50255cfeb0e232e178f80389440
Mamba1990 <h.daoudim@outlook.com>
2024-10-26 19:27:03 +0000

    pycodestyle-8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-'''Task 8's module.
-'''
+"""Lists all documents in a collection"""
 
 
 def list_all(mongo_collection):
-    '''Lists all documents in a collection.
-    '''
-    return [doc for doc in mongo_collection.find()]
+    """Return a list of all documents in a collection"""
+    return list(mongo_collection.find())
